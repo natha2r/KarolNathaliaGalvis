c01faad7e0518c87d805838bf66c18a72c447726
Nathalia Galvis <karolgalvis10@gmail.com>
2024-09-04 19:26:11 -0500

    Añadir Talleres

---
+++ Talleres/Taller2/public/resources/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <img src="/resources/logo.png" alt="Logo" />
+      <h2>LA MACETA FELIZ</h2>
+    </div>
+  );
+}
+
+export default Header;
